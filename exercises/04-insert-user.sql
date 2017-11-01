d2db0c97b2a922eaa9282154fcac8777d5d27d6f
Alyssa Mae Lim <adlim@up.edu.ph>
2017-11-01 16:45:24 +0800

    Update 04-insert-user.sql

--- exercises/04-insert-user.sql
+++ exercises/04-insert-user.sql
@@ -15,3 +15,5 @@
 --     result : SOME AWESOME QUERY email = 'arnellebalane@gmail.com';
 --
 -- Write your query below:
+
+INSERT INTO users (fullname, username, email, password) VALUES ('{{fullname}}', '{{username}}', '{{email}}', '{{password}}');
